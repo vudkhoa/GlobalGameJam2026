f5d804fb9b6979628ba04d6e217f6283f3413a8a
vudkhoa <vudinhkhoaowl@gmail.com>
2026-01-31 18:16:34 +0700

    Update: LoadingTask.

--- Assets/Core/Task/Scripts/FirstChapTask.cs
+++ Assets/Core/Task/Scripts/FirstChapTask.cs
@@ -2,9 +2,8 @@ using Cysharp.Threading.Tasks;
 
 public class FirstChapTask : BaseTask
 {
-    public override UniTask<bool> Execute()
+    public override UniTask Execute()
     {
-
         return UniTask.FromResult(true);
     }
 }
